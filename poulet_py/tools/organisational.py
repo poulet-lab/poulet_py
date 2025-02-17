2c6b98e647cf8626501152f4136fa7e692e9be11
Ivan <ivan.eromano@gmail.com>
2025-02-17 13:58:34 +0100

    two cameras

--- poulet_py/tools/organisational.py
+++ poulet_py/tools/organisational.py
@@ -18,16 +18,17 @@ def check_or_create(path):
 
 def define_folder_name(name):
     """
-    Defines a folder name by ensuring it only contains letters, numbers, and hyphens,
-    replacing invalid characters with hyphens, and appending the current date.
+    Defines a folder name by ensuring it only contains letters, numbers, and underscores,
+    replacing invalid characters with underscores, and prepending the current date in YYYYMMDD format.
 
     Parameters:
         name (str): The base name for the folder.
 
     Returns:
-        str: The formatted folder name.
+        str: The formatted folder name in the format YYYYMMDD_{name}.
     """
     name = str(name)
+    # Replace invalid characters with underscores
     name = "".join(e if e.isalnum() or e == "_" else "_" for e in name)
-    date = datetime.datetime.now().strftime("%y%m%d")
-    return f"{name}_{date}"
+    date = datetime.datetime.now().strftime("%Y%m%d")
+    return f"{date}_{name}"
