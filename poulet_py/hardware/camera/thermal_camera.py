b418f10d58b305d6fe3b817c1f074be5b48e0d21
Viktor Karamanis <viktor.karamanis@outlook.com>
2025-08-06 12:05:47 +0300

    remove comment

--- poulet_py/hardware/camera/thermal_camera.py
+++ poulet_py/hardware/camera/thermal_camera.py
@@ -9,10 +9,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
-# try:
-#     import keyboard
-# except ImportError:
-#     pass
+try:
+    import keyboard
+except ImportError:
+    pass
 try:
     from queue import Queue
 except ImportError:
