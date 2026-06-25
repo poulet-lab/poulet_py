97d769148888091d74afeccb72b45f2a10e71c52
Viktor Karamanis <viktor.karamanis@outlook.com>
2026-06-25 15:12:13 +0200

    fix example

--- examples/dcam.py
+++ examples/dcam.py
@@ -1,8 +1,8 @@
-# import cv2
-# import numpy as np
-
 import signal
 from time import sleep
+
+import cv2
+import numpy as np
 
 from poulet_py import DCAM
 
@@ -18,13 +18,13 @@ def handler(sig, frame):
 signal.signal(signal.SIGINT, handler)
 
 
-# def show_framedata(data):
-#     maxval = np.amax(data)
-#     if data.dtype == np.uint16 and maxval > 0:
-#         imul = int(65535 / maxval)
-#         data = data * imul
+def show_framedata(data):
+    maxval = np.amax(data)
+    if data.dtype == np.uint16 and maxval > 0:
+        imul = int(65535 / maxval)
+        data = data * imul
 
-#     cv2.imshow(NAME, data)
+    cv2.imshow(NAME, data)
 
 
 devices = DCAM.get_available_devices()
@@ -36,25 +36,20 @@ else:
 
 dcam = DCAM(device_index=0)
 
-try:
-    with dcam:
-        # cv2.namedWindow(
-        #     NAME,
-        #     cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO | cv2.WINDOW_GUI_NORMAL,
-        # )
+with dcam:
+    cv2.namedWindow(
+        NAME,
+        cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO | cv2.WINDOW_GUI_NORMAL,
+    )
 
-        while running:
-            sample = dcam.read_last_sample()
-            # print(sample)
-            # show_framedata(sample["dcam"])
+    while running:
+        sample = dcam.read_last_sample()
+        show_framedata(sample["dcam"])
 
-            # key = cv2.waitKey(1) & 0xFF
-            # if key == ord("q"):
-            #     break
-            sleep(0.01)
-            signal.pause()
+        signal.pause()
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord("q"):
+            break
+        sleep(0.01)
 
-    # cv2.destroyAllWindows()
-except KeyboardInterrupt:
-    print("\nShutdown initiated. Cleaning up resources...")
-    print("Shutdown complete.")
+cv2.destroyAllWindows()
